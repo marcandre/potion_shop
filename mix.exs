3f64a4c2afb07ff745c7a2a6fb6c9f5d056746a3
Marc-Andre Lafortune <github@marc-andre.ca>
2024-04-11 10:10:23 -0400

    Update ecto

--- mix.exs
+++ mix.exs
@@ -36,7 +36,7 @@ defmodule Carafe.MixProject do
       {:bcrypt_elixir, "~> 3.0"},
       {:phoenix, "~> 1.6.15"},
       {:phoenix_ecto, "~> 4.4"},
-      {:ecto_sql, "~> 3.6"},
+      {:ecto_sql, "~> 3.10"},
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 3.0"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
